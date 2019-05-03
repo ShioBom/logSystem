30d9b620596565f8d5b2c30c441d274c5a6c0f77
tangyu <15292061516@163.com>
2019-05-03 12:52:45 +0800

    工作日志页面发送请求

--- src/pages/WorkLog/index.vue
+++ src/pages/WorkLog/index.vue
@@ -2,7 +2,7 @@
   <div class="logger">
     <Header :title="title"></Header>
     <div class="content">
-      <mt-navbar v-model="selected">
+      <mt-navbar v-model="selected" value>
         <mt-tab-item id="1">我的日志</mt-tab-item>
         <mt-tab-item v-if="userStatus" id="2">员工日志</mt-tab-item>
         <mt-tab-item id="3">写日志</mt-tab-item>
@@ -11,17 +11,28 @@
       <mt-tab-container v-model="selected">
         <mt-tab-container-item id="1">
           <ul>
-            <LogItem v-for="(log, ind) in logs" :key="ind" :log="log"></LogItem>
+            <LogItem
+              v-for="(log, ind) in ownlogs"
+              :key="ind"
+              :log="log"
+            ></LogItem>
           </ul>
         </mt-tab-container-item>
         <mt-tab-container-item v-if="userStatus" id="2">
           <ul>
-            <LogItem v-for="(log, ind) in logs" :key="ind" :log="log"></LogItem>              
+            <LogItem v-for="(log, ind) in logs" :key="ind" :log="log"></LogItem>
           </ul>
         </mt-tab-container-item>
         <mt-tab-container-item id="3">
-         <mt-field placeholder="请输入日志" type="textarea" rows="20" v-model="logtext"></mt-field>
-         <mt-button type="default" size="large" @click="addLog()">发布日志</mt-button>
+          <mt-field
+            placeholder="请输入日志"
+            type="textarea"
+            rows="20"
+            v-model="logtext"
+          ></mt-field>
+          <mt-button type="default" size="large" @click="addLog()"
+            >发布日志</mt-button
+          >
         </mt-tab-container-item>
       </mt-tab-container>
     </div>
@@ -40,7 +51,7 @@ import TabContainerItem from "mint-ui/lib/tab-container-item";
 import "mint-ui/lib/tab-container-item/style.css";
 import InfiniteScroll from "mint-ui/lib/infinite-scroll";
 import "mint-ui/lib/infinite-scroll/style.css";
-import Field  from "mint-ui/lib/field";
+import Field from "mint-ui/lib/field";
 import "mint-ui/lib/field/style.css";
 
 Vue.use(Field);
@@ -55,59 +66,62 @@ export default {
   data() {
     return {
       title: "工作日志",
-      selected: "1",
+      selected: JSON.parse(sessionStorage("userInfo")).position!==0?true:false,
       userStatus: true,
-      logs: [
-        {
-          comment_id: 0, //评论id   0=没有评论
-          log_context:
-            "成年人自己要给自己拥抱，当你有力量拥抱自己的时候，你才有力量拥抱所有人。", //内容
-          log_date: "2019-04-30 14:30:07.0", //发布日志时间
-          log_id: 10018, //日志id
-          status: 0, //日志状态  0=未被阅读
-          uname: "alin" //写日志的用户名
-        },
-        {
-          comment_id: 0, //评论id   0=没有评论
-          log_context:
-            "成年人自己要给自己拥抱，当你有力量拥抱自己的时候，你才有力量拥抱所有人。", //内容
-          log_date: "2019-04-30 14:30:07.0", //发布日志时间
-          log_id: 10018, //日志id
-          status: 0, //日志状态  0=未被阅读
-          uname: "alin" //写日志的用户名
-        },
-        {
-          comment_id: 0, //评论id   0=没有评论
-          log_context:
-            "成年人自己要给自己拥抱，当你有力量拥抱自己的时候，你才有力量拥抱所有人。", //内容
-          log_date: "2019-04-30 14:30:07.0", //发布日志时间
-          log_id: 10018, //日志id
-          status: 0, //日志状态  0=未被阅读
-          uname: "alin" //写日志的用户名
-        },
-        {
-          comment_id: 0, //评论id   0=没有评论
-          log_context:
-            "成年人自己要给自己拥抱，当你有力量拥抱自己的时候，你才有力量拥抱所有人。", //内容
-          log_date: "2019-04-30 14:30:07.0", //发布日志时间
-          log_id: 10018, //日志id
-          status: 0, //日志状态  0=未被阅读
-          uname: "alin" //写日志的用户名
-        }
+      ownlogs: [
       ],
-      logtext:""
+      logs: [],
+      logtext: ""
     };
   },
   components: { Header, LogItem },
   methods: {
-    addLog(){
-        console.log(this.logtext);
+    //添加日志
+    addLog() {
+      console.log("日志内容", this.logtext);
+      let params = new URLSearchParams();
+      params.append("uname", this.user.uname);
+      params.append("log_context", this.logtext);
+      this.$axios.post("LogSystem/addlog", params).then(res => {
+        if (res.data.keycode === 200) {
+          Toast(res.data.message);
+        }
+      });
+    },
+    //获取自己的日志
+    queryOwnLogs() {
+      let params = new URLSearchParams();
+      params.append("uname", this.user.uname);
+      this.$axios.post("LogSystem/findlogbyname", params).then(res => {
+        if (res.data.keycode === 200) {
+          this.ownlogs = res.data.data;
+        }
+      });
+    },
+    //获取员工的日志
+    queryStaffLogs() {
+      let params = new URLSearchParams();
+      params.append("uname", this.user.uname);
+      this.$axios.post("LogSystem/findlogbystatus", params).then(res => {
+        if (res.data.keycode === 200) {
+          this.logs = res.data.data;
+        }
+      });
     }
   },
   mounted() {
+    this.queryOwnLogs();
     console.log(this.logtext);
     //初始化滚动条
     setTimeout(() => {}, 1000);
+  },
+  updated() {
+    if (this.selected === 1) {
+      this.queryOwnLogs();
+    } else if (this.selected === 2) {
+      this.queryStaffLogs();//请求员工日志数据
+    }
+    console.log(this.selected);
   }
 };
 </script>
@@ -125,15 +139,14 @@ export default {
   left: 0;
   bottom: 0;
   right: 0;
-  .mint-tab-container{
-      height: 5.61rem;
+  .mint-tab-container {
+    height: 5.61rem;
   }
-  .mint-tab-container-item{
-      max-height: 5.61rem;
-      overflow-y: auto;
+  .mint-tab-container-item {
+    max-height: 5.61rem;
+    overflow-y: auto;
   }
 }
-
 </style>
 <style lang="scss">
 .mint-navbar .mint-tab-item.is-selected {
